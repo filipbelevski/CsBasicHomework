036797501b260ad97d730e592fbef08fcf92825c
filipbelevski <filip.belevski@gmail.com>
2021-03-31 19:16:07 +0200

    Class03.ExtraHomework

---
+++ Class03/SEDC.BasicOop.Class03.ExtraHomework/SEDC.BasicOop.Class03.ExtraHomework.Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SEDC.BasicOop.Class03.ExtraHomework.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that will print out all numbers from the range 1 - N(N is input from keyboard, N > 2) that divide with 3.
+
+            Console.WriteLine("Please enter a number bigger than 2");
+            bool isValidInput = int.TryParse(Console.ReadLine(), out int input);
+
+            if (isValidInput)
+            {
+                if (input > 2)
+                {
+                    for (int i = 0; i < input; i++)
+                    {
+                        if (i % 3 != 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine(i + " divides with 3");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number bigger than 2");
+            }
+        }
+    }
+}
