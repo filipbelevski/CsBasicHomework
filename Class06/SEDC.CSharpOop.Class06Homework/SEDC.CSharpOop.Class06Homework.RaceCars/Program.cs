17bcad9a2ec2b9b09e73c97318b3c686f6c1c303
filipbelevski <filip.belevski@gmail.com>
2021-04-09 08:16:06 +0200

    RaceCars Refactor

--- Class06/SEDC.CSharpOop.Class06Homework/SEDC.CSharpOop.Class06Homework.RaceCars/Program.cs
+++ Class06/SEDC.CSharpOop.Class06Homework/SEDC.CSharpOop.Class06Homework.RaceCars/Program.cs
@@ -8,6 +8,10 @@ namespace SEDC.CSharpOop.Class06Homework.RaceCars
     {
         static void Main(string[] args)
         {
+        }
+        // RaceApp Core
+        static void RaceApp()
+        {
             Car[] cars = new Car[]
             {
                 new Car("Hyundai", 20),
@@ -23,119 +27,170 @@ namespace SEDC.CSharpOop.Class06Homework.RaceCars
                 new Driver("Jill", 9),
                 new Driver("Anne", 10),
             };
-            Car firstCar;
-            while (true)
+
+            // initializing classes to store car and driver obj
+            Car firstCar = new Car();
+            Car secondCar = new Car();
+            Driver firstDriver = new Driver();
+            Driver secondDriver = new Driver();
+            // initializing counters and userInput, so we can dynamically change the choice #; car #1, driver #2;
+            int carPrompt = 1;
+            int driverPrompt = 1;
+            int userCarChoiceInput = 0;
+            int userDriverChoiceInput = 0;
+            
+            // propmpt user car choice and check if userInput is Valid//
+            PromptUserCarChoice(carPrompt, cars, firstCar);
+            userCarChoiceInput = ReadUserChoiceAndValidate(Console.ReadLine(), cars);
+            while (userCarChoiceInput == 0) // Validation returns 0 if invalid, else we get the valid userInput and set properties to the car object
             {
-                while (true)
-                {
-                    Console.WriteLine("Choose a car no.1, by entering the car id ");
-                    for (int i = 0; i < cars.Length; i++)
-                    {
-                        Console.WriteLine($"#{i + 1}: {cars[i].Model}");
-                    }
-                    bool IsValidInputFirstCar = int.TryParse(Console.ReadLine(), out int carSelectionId);
-                    if (IsValidInputFirstCar && carSelectionId < 5)
-                    {
-                        firstCar = cars[carSelectionId - 1];
-                        break;
-                    }
-                }
-                while (true)
-                {
-                    Console.WriteLine("Choose a driver no.1, by entering the driver id");
-                    for (int i = 0; i < drivers.Length; i++)
-                    {
-                        Console.WriteLine($"#{i + 1}: {drivers[i].Name}");
-                    }
-                    bool IsValidInputFirstCar = int.TryParse(Console.ReadLine(), out int driverSelectionId);
-                    if (IsValidInputFirstCar && driverSelectionId < 5)
-                    {
-                        firstCar.Driver = drivers[driverSelectionId - 1];
-                        break;
-                    }
-                }
-                Car secondCar;
-                while (true)
-                {
-                    Console.WriteLine("Choose a car no.2, by entering the car id");
-                    for (int i = 0; i < cars.Length; i++)
-                    {
-                        if (cars[i].Model == firstCar.Model)
-                        {
-                            continue;
-                        }
-                        Console.WriteLine($"#{i + 1}: {cars[i].Model}");
-                    }
-                    bool IsValidSecondCar = int.TryParse(Console.ReadLine(), out int carSelectionId);
-                    if (IsValidSecondCar && carSelectionId < 5)
-                    {
-                        secondCar = cars[carSelectionId - 1];
-                        if (secondCar.Model == firstCar.Model)
-                        {
-                            Console.WriteLine("This car is already used, please select another car");
-                        }
-                        else
-                        {
-                            break;
-                        }
-
-                    }
-                }
-                while (true)
-                {
-                    Console.WriteLine("Choose a driver no.2, by entering the driver id");
-                    for (int i = 0; i < drivers.Length; i++)
-                    {
-                        if(drivers[i].Name == firstCar.Driver.Name)
-                        {
-                            continue;
-                        }
-                        Console.WriteLine($"#{i + 1}: {drivers[i].Name}");
-                    }
-                    bool IsValidInputSecondCar = int.TryParse(Console.ReadLine(), out int driverSelectionId);
-                    if (IsValidInputSecondCar && driverSelectionId < 5)
-                    {
-                        secondCar.Driver = drivers[driverSelectionId - 1];
-                        if (secondCar.Driver == firstCar.Driver)
-                        {
-                            Console.WriteLine("This driver is already selected, please select another driver");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                RaceCars(firstCar, secondCar);
-
-                Console.WriteLine("Enter Y / N to race again or to exit the app");
-                if (!(Console.ReadLine().ToLower() == "Y".ToLower()))
-                {
-                    break;
-                }
-
+                PromptUserCarChoice(carPrompt, cars, firstCar);
+                userCarChoiceInput = ReadUserChoiceAndValidate(Console.ReadLine(), cars);
+                firstCar = UserCarChoice(cars, userCarChoiceInput);
             }
-
-            static void RaceCars(Car firstCar, Car secondCar)
+            carPrompt++;
+            //propmpt user driver choice and check if userInput is Valid//
+            PromptUserDriverChoice(driverPrompt, drivers, firstDriver);
+            userDriverChoiceInput = ReadUserChoiceAndValidateDriver(Console.ReadLine(), drivers); 
+            while (userDriverChoiceInput == 0) //Validation returns 0 if invalid, else we get the valid userInput and set properties to the Driver object
             {
-                int firstCarSpeed = firstCar.CalculateSpeed();
-                int secondCarSpeed = secondCar.CalculateSpeed();
+                PromptUserDriverChoice(driverPrompt, drivers, firstDriver);
+                userDriverChoiceInput = ReadUserChoiceAndValidateDriver(Console.ReadLine(), drivers);
+            }
+            firstDriver = UserDriverChoice(drivers, userDriverChoiceInput);
+            firstCar.Driver = firstDriver;
+            driverPrompt++;
 
-                if (firstCarSpeed > secondCarSpeed)
+            PromptUserCarChoice(carPrompt, cars, firstCar);
+            userCarChoiceInput = ReadUserChoiceAndValidate(Console.ReadLine(), cars);
+            while(userCarChoiceInput == 0)
+            {
+                PromptUserCarChoice(carPrompt, cars, firstCar);
+                int tempInput = ReadUserChoiceAndValidate(Console.ReadLine(), cars);
+                secondCar = UserCarChoice(cars, tempInput);
+                if(CarInUse(firstCar, secondCar))
                 {
-                    Console.WriteLine($"{firstCar.Driver.Name} won the race driving a {firstCar.Model}. They were going {firstCarSpeed}");
-                }
-                else if (firstCarSpeed < secondCarSpeed)
-                {
-                    Console.WriteLine($"{secondCar.Driver.Name} won the race driving a {secondCar.Model}. They were going {secondCarSpeed}");
-                }
-                else
-                {
-                    Console.WriteLine($"What a close race by {firstCar.Driver.Name} and {secondCar.Driver.Name} it's A TIE! ");
+                    userCarChoiceInput = 0;
                 }
             }
 
+            PromptUserDriverChoice(driverPrompt, drivers, firstDriver);
+            userDriverChoiceInput = ReadUserChoiceAndValidateDriver(Console.ReadLine(), drivers);
+            while (userDriverChoiceInput == 0)
+            {
+                PromptUserDriverChoice(driverPrompt, drivers, firstDriver);
+                int tempInput = ReadUserChoiceAndValidateDriver(Console.ReadLine(), drivers);
+                if(tempInput == userDriverChoiceInput)
+                {
+                    userDriverChoiceInput = 0;
+                }
+            }
+            secondDriver = UserDriverChoice(drivers, userDriverChoiceInput);
+            secondCar.Driver = secondDriver;
+
+
+            RaceCars(firstCar, secondCar);
+
+        }
+        // Methods For RaceApp
+        static void PromptUserDriverChoice(int counter, Driver[]driverArray, Driver someDriver)
+        {
+            Console.WriteLine($"Select driver no.{counter}");
+            LogDrivers(driverArray, someDriver);
+        }
+        static void PromptUserCarChoice (int counter, Car[] carArray, Car someCar)
+        {
+            Console.WriteLine($"Select car no.{counter}");
+            LogCars(carArray, someCar);
+        }
+        static bool CarInUse (Car firstCar, Car secondCar)
+        {
+            if (firstCar.Model == secondCar.Model)
+            {
+                return true;
+            }
+            else
+            {
+
+            return false;
+            }
+        }
+        static void LogCars (Car[] carArray, Car firstCar)
+        {
+            for(int i = 0; i < carArray.Length; i++)
+            {
+                if (carArray[i].Model == firstCar.Model)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{i+1}: {carArray[i].Model}");
+            }
+        }
+        static void LogDrivers (Driver[] driverArray, Driver firstDriver)
+        {
+            for (int i = 0; i < driverArray.Length; i++)
+            {
+                if (driverArray[i].Name == firstDriver.Name)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{i+1}: {driverArray[i].Name}");
+            }
+        }
+        static int ReadUserChoiceAndValidateDriver(string userInput, Driver[] driverArray)
+        {
+          
+            bool isValidInput = int.TryParse(userInput, out int userChoice);
+
+            if (isValidInput && userChoice <= driverArray.Length && userChoice > 0)
+            {
+                return userChoice;
+            }
+
+            return 0;
+
+        }
+
+        static int ReadUserChoiceAndValidate(string userInput, Car[] carArray)
+        {
+            bool isValidInput = int.TryParse(userInput, out int userChoice);
+
+            if (isValidInput && userChoice <= carArray.Length && userChoice > 0)
+            {
+                return userChoice;
+            }
+
+            return 0;
+        }
+        static Driver UserDriverChoice(Driver[] driverArray, int userInput)
+        {
+            Driver driverChoice = driverArray[userInput - 1];
+
+            return driverChoice;
+        }
+        static Car UserCarChoice(Car[] carArray, int userInput)
+        {
+            Car carChoice = carArray[userInput - 1];
+
+            return carChoice;
+        }
+        static void RaceCars(Car firstCar, Car secondCar)
+        {
+            int firstCarSpeed = firstCar.CalculateSpeed();
+            int secondCarSpeed = secondCar.CalculateSpeed();
+
+            if (firstCarSpeed > secondCarSpeed)
+            {
+                Console.WriteLine($"{firstCar.Driver.Name} won the race driving a {firstCar.Model}. They were going {firstCarSpeed}");
+            }
+            else if (firstCarSpeed < secondCarSpeed)
+            {
+                Console.WriteLine($"{secondCar.Driver.Name} won the race driving a {secondCar.Model}. They were going {secondCarSpeed}");
+            }
+            else
+            {
+                Console.WriteLine($"What a close race by {firstCar.Driver.Name} and {secondCar.Driver.Name} it's A TIE! ");
+            }
         }
     }
 }
---
+++ Class06/SEDC.CSharpOop.Class06Homework/SEDC.CSharpOop.Class06HomeworkF/Program.cs
@@ -0,0 +1,176 @@
+﻿using SEDC.CSharpOop.Class06Homework.RaceCars.MyClasses;
+using System;
+
+
+namespace SEDC.CSharpOop.Class06Homework.RaceCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RaceApp();
+            Console.WriteLine("Enter Y to race again");
+            if(Console.ReadLine().ToLower() == "y")
+            {
+                Console.Clear();
+                RaceApp();
+            }
+            Console.ReadLine();
+
+        }
+
+        static void RaceApp()
+        {
+            Car[] cars = new Car[]
+            {
+                new Car("Hyundai", 20),
+                new Car("Mazda", 20),
+                new Car("Ferrari", 24),
+                new Car("Porsche", 22),
+            };
+            Driver[] drivers = new Driver[]
+            {
+                new Driver("Bob", 9),
+                new Driver("Greg", 8),
+                new Driver("Jill", 9),
+                new Driver("Anne", 10),
+            };
+
+            Car firstCar = new Car();
+            Car secondCar = new Car();
+            Driver firstDriver = new Driver();
+            Driver secondDriver = new Driver();
+
+            Console.WriteLine("Enter car no.1");
+            LoadCarsExceptUsed(cars, firstCar);
+            int selectionFirstCar = IsValidSelectedCar(Console.ReadLine(), cars, firstCar, secondCar);
+            while (selectionFirstCar == 0)
+            {
+                Console.WriteLine("Select a valid car");
+                LoadCarsExceptUsed(cars, firstCar);
+                selectionFirstCar = IsValidSelectedCar(Console.ReadLine(), cars, firstCar, secondCar);
+            }
+            firstCar = cars[selectionFirstCar - 1];
+
+            Console.WriteLine("Enter driver no.2");
+            LoadDriversExceptUsed(drivers, firstDriver);
+            int selectionFirstDriver = IsValidSelectedDriver(Console.ReadLine(), drivers, firstDriver, secondDriver);
+            while (selectionFirstDriver == 0)
+            {
+                Console.WriteLine("Select a valid driver");
+                LoadDriversExceptUsed(drivers, firstDriver);
+                selectionFirstDriver = IsValidSelectedDriver(Console.ReadLine(), drivers, firstDriver, secondDriver);
+            }
+            firstDriver = drivers[selectionFirstDriver - 1];
+
+            Console.WriteLine("Enter car no.2");
+            LoadCarsExceptUsed(cars, firstCar);
+            int selectionSecondCar = IsValidSelectedCar(Console.ReadLine(), cars, firstCar, secondCar);
+            while(selectionSecondCar == 0)
+            {
+                Console.WriteLine("Select a valid car");
+                LoadCarsExceptUsed(cars, firstCar);
+                selectionSecondCar = IsValidSelectedCar(Console.ReadLine(), cars, firstCar, secondCar);
+            }
+            secondCar = cars[selectionSecondCar - 1];
+
+            Console.WriteLine("Enter driver no.2");
+            LoadDriversExceptUsed(drivers, firstDriver);
+            int selectionSecondDriver = IsValidSelectedDriver(Console.ReadLine(), drivers, firstDriver, secondDriver);
+            while(selectionSecondDriver == 0)
+            {
+                Console.WriteLine("Select a valid driver");
+                LoadDriversExceptUsed(drivers, firstDriver);
+                selectionSecondDriver = IsValidSelectedDriver(Console.ReadLine(), drivers, firstDriver, secondDriver);
+            }
+            secondDriver = drivers[selectionSecondDriver - 1];
+
+            firstCar.Driver = firstDriver;
+            secondCar.Driver = secondDriver;
+
+            RaceCars(firstCar, secondCar);
+
+        }
+        static int IsValidSelectedDriver(string userInput, Driver[] driverArray, Driver firstDriver, Driver secondDriver)
+        {
+            bool isValidSelection = int.TryParse(userInput, out int selection);
+            if (isValidSelection && selection > 0 && selection < 5)
+            {
+                secondDriver = driverArray[selection - 1];
+                if (firstDriver.Name == secondDriver.Name)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return selection;
+                }
+
+            }
+            else
+            {
+                return 0;
+            };
+        }
+        static int IsValidSelectedCar(string userInput, Car[] carArray, Car firstCar, Car secondCar)
+        {
+            bool isValidSelection = int.TryParse(userInput, out int selection);
+            if (isValidSelection && selection > 0 && selection < 5)
+            {
+                secondCar = carArray[selection - 1];
+                if(firstCar.Model == secondCar.Model)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return selection;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void LoadDriversExceptUsed(Driver [] driverArray, Driver firstDriver)
+        {
+            for (int i = 0; i < driverArray.Length; i++)
+            {
+                if (driverArray[i].Name == firstDriver.Name)
+                {
+                    continue;
+                }
+                Console.WriteLine($"#{i + 1}: {driverArray[i].Name}, Skill: {driverArray[i].Skill}");
+            }
+        }
+        static void LoadCarsExceptUsed(Car[] carArray, Car firstCar)
+        {
+            for (int i = 0; i < carArray.Length; i++)
+            {
+                if (carArray[i].Model == firstCar.Model)
+                {
+                    continue;
+                }
+                Console.WriteLine($"#{i + 1}: {carArray[i].Model}, Speed: {carArray[i].Speed}");
+            }
+        }
+        static void RaceCars(Car firstCar, Car secondCar)
+        {
+            int firstCarSpeed = firstCar.CalculateSpeed();
+            int secondCarSpeed = secondCar.CalculateSpeed();
+
+            if (firstCarSpeed > secondCarSpeed)
+            {
+                Console.WriteLine($"{firstCar.Driver.Name} won the race driving a {firstCar.Model}. They were going {firstCarSpeed}");
+            }
+            else if (firstCarSpeed < secondCarSpeed)
+            {
+                Console.WriteLine($"{secondCar.Driver.Name} won the race driving a {secondCar.Model}. They were going {secondCarSpeed}");
+            }
+            else
+            {
+                Console.WriteLine($"What a close race by {firstCar.Driver.Name} and {secondCar.Driver.Name} it's A TIE! ");
+            }
+        }
+    }
+}
